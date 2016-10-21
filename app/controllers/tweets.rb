64bb814c35d9add9c6b363475835945e87f5bc62
Frank Sommers <email": null>
2016-10-21 13:41:17 -0700

    recommented redirects after deploying to heroku

--- app/controllers/tweets.rb
+++ app/controllers/tweets.rb
@@ -1,15 +1,15 @@
 get '/app' do
   @map_key=ENV['MAP_KEY']
   @tweets = Tweet.all
-  # if !logged_in?
+  if !logged_in?
      redirect '/login'
 
-  # elsif request.xhr?
-  # 	trump_stream
-  #   @trump_stream_array.to_json
-  # else
+  elsif request.xhr?
+  	trump_stream
+    @trump_stream_array.to_json
+  else
     erb :"tweets/app"
-  # end
+  end
 end
 
 
